b7d8e95a8e9fb00a297af9c1c6bb87054f12b3b0
huzaifa-mern-dev <mhuzaifa3737@gmail.com>
2024-10-14 19:24:37 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E201E",
+        secondary: {
+          DEFAULT: "#AD49E1",
+          100: "#AD49E1",
+          200: "#AD49E1",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#181C14",
+          200: "#181C14",
+        },
+        gray: {
+          100: "#EBD3F8",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
